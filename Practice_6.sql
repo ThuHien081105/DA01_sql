1938ccb3f9cf99f9f9683d3db34545bf858c0922
ThuHien081105 <hienbui081105@gmail.com>
2025-09-05 21:53:23 +0700

    Practice_6.sql

--- Practice_6.sql
+++ Practice_6.sql
@@ -27,5 +27,22 @@ WHERE b.page_id IS NULL
 ORDER BY a.page_id
 --Exercise5 (Khó quá e chưa hiểu vs chưa làm được ạ)
 --Exercise6
+SELECT 
+DATE_FORMAT(trans_date, '%Y-%m') AS month, country,
+COUNT(*) AS trans_count,
+SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY country, DATE_FORMAT(trans_date, '%Y-%m')
+ORDER BY month;
+--Exercise7
+SELECT product_id, first_year, quantity, price
+FROM(SELECT product_id, quantity, price, year,
+FIRST_VALUE(year) OVER (PARTITION BY product_id ORDER BY year) AS first_year
+FROM Sales
+) a
+WHERE year = first_year;
+--Exercise8
 
 
