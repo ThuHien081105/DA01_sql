43627b2b8e0300d9fb8e19fbd84372c1bfdcfaec
ThuHien081105 <hienbui081105@gmail.com>
2025-09-09 19:32:36 +0700

    Practice_6.sql

--- Practice_6.sql
+++ Practice_6.sql
@@ -44,5 +44,51 @@ FROM Sales
 ) a
 WHERE year = first_year;
 --Exercise8
-
+SELECT a.customer_id FROM Customer AS a
+JOIN Product AS b
+ON a.product_key = b.product_key
+WHERE b.product_key IN ('5','6')
+GROUP BY a.customer_id
+HAVING COUNT(DISTINCT b.product_key) = 2;
+--Exercise9
+SELECT e.employee_id FROM Employees e
+WHERE e.salary < 30000
+AND e.manager_id IS NOT NULL
+AND e.manager_id NOT IN (
+SELECT employee_id FROM Employees)
+ORDER BY e.employee_id;
+--Exercise10
+SELECT COUNT(*)  AS duplicate_companies
+FROM (SELECT company_id 
+FROM job_listings
+GROUP BY company_id, title
+HAVING COUNT(*) >1) a
+--Exercise11
+WITH c AS (
+SELECT a.name, COUNT(b.movie_id) AS total_ratings FROM Users a
+JOIN MovieRating b ON a.user_id = b.user_id 
+GROUP BY a.name),
+feb_ratings AS (SELECT m.title, AVG(r.rating) AS avg_rating
+FROM Movies m
+JOIN MovieRating r ON m.movie_id = r.movie_id
+WHERE r.created_at >= '2020-02-01'
+AND r.created_at < '2020-03-01'
+GROUP BY m.title)
+(SELECT name  AS results FROM c
+ORDER BY total_ratings DESC, name ASC
+LIMIT 1) 
+UNION ALL
+(SELECT title  AS results FROM feb_ratings
+ORDER BY avg_rating DESC, title ASC
+LIMIT 1)
+--Exercise12
+SELECT id, COUNT(DISTINCT friend_id) AS num
+FROM (SELECT requester_id AS id, accepter_id AS friend_id
+FROM RequestAccepted
+UNION ALL
+SELECT accepter_id AS id, requester_id AS friend_id
+FROM RequestAccepted) AS all_friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
 
